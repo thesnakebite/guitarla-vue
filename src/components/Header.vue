54533c19ed982d03c5c9c37c11b332586e9a2e62
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-09-25 17:47:15 +0200

    Calculating the total to pay with a computed property

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import { computed } from 'vue';
+
     const props = defineProps({
         carrito: {
             type: Array,
@@ -11,6 +13,10 @@
     })
 
     defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito'])
+
+    const totalPagar = computed( () => {
+        return props.carrito.reduce((total, producto) => total + (producto.cantidad * producto.precio), 0)
+    })
 </script>
 
 <template>
@@ -85,7 +91,7 @@
                                         </tr>
                                     </tbody>
                                 </table>
-                                <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
+                                <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
                                 <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                             </div>
                         </div>
